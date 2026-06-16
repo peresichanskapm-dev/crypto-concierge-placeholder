b6847a57050285f84de7baf806d022e68e8b3fc2
Tochechka <zarytskyi.oleksandr.i@gmail.com>
2026-06-16 14:02:33 +0300

    deploy: add out files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
+    basePath: '/placeholder',
+    assetPrefix: '/placeholder/',
     images: {
         unoptimized: true,
     },
--- out/_next/static/QE6x84lNlg0kv32Jv1Qq0/_buildManifest.js
+++ out/_next/static/QE6x84lNlg0kv32Jv1Qq0/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/placeholder//_next/:path+",
+        "destination": "/placeholder/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
