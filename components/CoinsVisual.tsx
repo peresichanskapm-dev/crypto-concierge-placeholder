200fb0ba5ad080177fed9a04190084790e71c1e3
Tochechka <zarytskyi.oleksandr.i@gmail.com>
2026-06-16 12:20:32 +0300

    first commit

---
+++ components/CoinsVisual.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export default function CoinsVisual() {
+    return (
+        <div className="absolute inset-0 z-0 w-full h-full max-w-7xl mx-auto pointer-events-none">
+            <div className="absolute -bottom-10 left-1/2 -translate-x-1/2 md:bottom-0 md:right-0 md:left-auto md:translate-x-0 w-[280px] md:w-[450px]">
+                <Image
+                    src="/images/coins-stack.png"
+                    alt="Криптовалютні монети"
+                    width={500}
+                    height={500}
+                    className="w-full h-auto object-contain opacity-90 md:opacity-100 mix-blend-screen md:mix-blend-normal"
+                    priority
+                />
+            </div>
+        </div>
+    )
+}
