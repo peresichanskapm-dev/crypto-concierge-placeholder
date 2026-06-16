12453da9a53cecc37ee6a1fcfdb81b9c42144abe
Tochechka <zarytskyi.oleksandr.i@gmail.com>
2026-06-16 14:19:06 +0300

    deploy: fix image path

--- components/CoinsVisual.tsx
+++ components/CoinsVisual.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
-import coinsImage from '../public/images/coins-stack.png'
 
 export default function CoinsVisual() {
     return (
         <div className="absolute inset-0 z-0 w-full h-full max-w-7xl mx-auto pointer-events-none">
             <div className="absolute -bottom-10 left-1/2 -translate-x-1/2 md:bottom-0 md:right-0 md:left-auto md:translate-x-0 w-[280px] md:w-[450px]">
                 <Image
-                    src={coinsImage}
+                    src="/placeholder/images/coins-stack.png"
                     alt="Криптовалютні монети"
                     width={500}
                     height={500}
